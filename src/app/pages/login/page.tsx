f2f75bc441baf2ba87009fc365beb135ed7d1c64
DasFiel <jeanhsantosfiel07@gmail.com>
2025-10-17 16:48:18 -0300

    page login prototipo 2

--- src/app/pages/login/page.tsx
+++ src/app/pages/login/page.tsx
@@ -1,15 +1,17 @@
 "use client";
 import styles from "./styles.module.css";
 import FormLogin from "../../../components/FormLogin";
+import WelcomeLogin from "@/components/WelcomeLogin";
 
 export default function Page() {
   return (
     <div className={styles.container}>
       <div className={styles.painelBoasVindas}>
-        olá
-        <span className={styles.logotipo}>Logotipo</span>
+        
+        <span className={styles.logotipo}></span>
       </div>
       <div className={styles.painelLogin}>
+        <WelcomeLogin />
         <FormLogin />
       </div>
     </div>
--- src/components/FormLogin/index.tsx
+++ src/components/FormLogin/index.tsx
@@ -1,18 +1,52 @@
 import styles from "./styles.module.css";
 import React from "react";
-import FormTextPassword from "@mui/material/TextField";
-import FormTextEmail from "../FormTextEmail";
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Form from 'react-bootstrap/Form';
+
+import Button from 'react-bootstrap/Button';
+import Image from "next/image";
+
 
 
 export default function FormLogin() {
   return (
     <form className={styles.container}>
-      <span className={styles.logotipo}>Logotipo</span>
+      <span className={styles.logotipo}><Image src="/imgs/logo.png" alt="logo do trem" fill style={{objectFit:"contain"}} /></span>
       <h2>Bem-vinda de volta!</h2>
-
-      <FormTextPassword />
-      <FormTextEmail />
-      
+      <FormTextLogin />
+      <ButtonLogin />
     </form>
+  );
+}
+
+function FormTextLogin() {
+  return (
+    <>
+      <FloatingLabel
+        controlId="floatingInput"
+        label="Email address"
+        className="mb-3"
+      >
+        <Form.Control type="email" placeholder="name@example.com" />
+      </FloatingLabel>
+
+      <FloatingLabel 
+      controlId="floatingPassword" 
+      label="Password"
+      className="mb-3"
+      >
+        <Form.Control type="password" placeholder="Password" />
+      </FloatingLabel>
+    </>
+  );
+}
+
+function ButtonLogin() {
+  return (
+    <div className="d-grid gap-2">
+      <Button variant="primary" size="lg">
+        Entrar
+      </Button>
+    </div>
   );
 }

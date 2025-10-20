01f61646db403d0c5a7849a272b31addb16a4c4d
Gustavo Braga <agbram2005@gmail.com>
2025-10-20 16:32:23 -0300

    alterações no login

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./globals.css";
+import { AuthProvider } from "@/contexts/AuthContext";
+import PrivateRouter from "@/components/PrivateRouter";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +30,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+      
+        <AuthProvider>
         {children}
+        </AuthProvider>
+        
       </body>
     </html>
   );

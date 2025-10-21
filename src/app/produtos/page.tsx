af6620ae6671b6295e571879770351288b7e891e
brunorudalov <brudalov@gmail.com>
2025-10-21 14:28:44 -0300

    page produtos

--- src/app/produtos/page.tsx
+++ src/app/produtos/page.tsx
@@ -1,8 +1,7 @@
 'Use Client';
 
 import Header from "@/components/Header";
-import ProductList from "@/components/teste/TestesProdut";
-import produtos from "@/mockstestes/proTestes";
+;
 
 
 export default function ProdutosTela() {
